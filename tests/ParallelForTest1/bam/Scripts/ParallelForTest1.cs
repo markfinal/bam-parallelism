1d95857574a6f193f4e94d14b800332de328b97d
Mark Final <markfinal@hotmail.com>
2019-06-22 13:20:45 +0100

    [tests,CI] Add a TBB parallel_for test, and enable CI

---
+++ tests/ParallelForTest1/bam/Scripts/ParallelForTest1.cs
@@ -0,0 +1,83 @@
+#region License
+// Copyright (c) 2010-2019, Mark Final
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of BuildAMation nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#endregion // License
+using Bam.Core;
+namespace ParallelForTest1
+{
+    class ParallelForTest1 :
+        C.Cxx.ConsoleApplication
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            var source = this.CreateCxxSourceContainer("$(packagedir)/source/*.cpp");
+            this.CompileAndLinkAgainst<tbb.ThreadBuildingBlocks>(source);
+
+            source.PrivatePatch(settings =>
+            {
+                var compiler = settings as C.ICommonCompilerSettings;
+                compiler.WarningsAsErrors = true;
+
+                if (settings is VisualCCommon.ICommonCompilerSettings vcCompiler)
+                {
+                    vcCompiler.WarningLevel = VisualCCommon.EWarningLevel.Level4;
+                }
+                else if (settings is GccCommon.ICommonCompilerSettings gccCompiler)
+                {
+                    gccCompiler.AllWarnings = true;
+                    gccCompiler.ExtraWarnings = true;
+                    gccCompiler.Pedantic = true;
+                }
+                else if (settings is ClangCommon.ICommonCompilerSettings clangCompiler)
+                {
+                    clangCompiler.AllWarnings = true;
+                    clangCompiler.ExtraWarnings = true;
+                    clangCompiler.Pedantic = true;
+                }
+            });
+        }
+    }
+
+    sealed class TBBRuntime :
+        Publisher.Collation
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.SetDefaultMacrosAndMappings(EPublishingType.ConsoleApplication);
+            this.Include<ParallelForTest1>(C.Cxx.ConsoleApplication.ExecutableKey);
+        }
+    }
+}
---
+++ tests/ParallelForTest1/source/main.cpp
@@ -0,0 +1,100 @@
+/*
+Copyright (c) 2010-2019, Mark Final
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of BuildAMation nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#include "tbb/tbb.h"
+
+#include <vector>
+#include <iostream>
+
+struct Task
+{
+    Task(const int inLimit)
+        :
+        _limit(inLimit)
+    {}
+
+    void operator()() const
+    {
+        std::cout << tbb::this_tbb_thread::get_id() << "## : " << std::endl;
+        for (int i = 0; i < this->_limit; ++i)
+        {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    int _limit;
+};
+
+struct Executor
+{
+    Executor(const std::vector<Task> &inTasks)
+        :
+        _tasks(inTasks)
+    {}
+
+    void operator()(const tbb::blocked_range<size_t>& inRange) const
+    {
+        for (size_t it = inRange.begin(); it != inRange.end(); ++it)
+        {
+            this->_tasks[it]();
+        }
+    }
+
+    const std::vector<Task> &_tasks;
+};
+
+int main()
+{
+    tbb::task_scheduler_init init; // defaults
+
+    std::vector<Task> tasks;
+    for (int i = 0; i < 10; ++i)
+    {
+        tasks.push_back(Task(i));
+    }
+
+    Executor exec(tasks);
+
+    std::cout << tbb::this_tbb_thread::get_id() << "## Starting parallel_for..." << std::endl;
+#ifdef TBB_PREVIEW_SERIAL_SUBSET
+    tbb::serial::parallel_for(
+        tbb::blocked_range<size_t>(0, tasks.size()),
+        exec
+    );
+#else
+    tbb::parallel_for(
+        tbb::blocked_range<size_t>(0, tasks.size()),
+        exec
+    );
+#endif
+    std::cout << tbb::this_tbb_thread::get_id() << "## Finished parallel_for" << std::endl;
+
+    return 0;
+}
